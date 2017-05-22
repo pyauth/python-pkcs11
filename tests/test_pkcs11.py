7dfaefafdd5fedcff98d274a37f1ba997d2bec4c
Danielle Madeley <danielle.madeley@squareweave.com.au>
2017-05-22 15:45:30 +1000

    Add test for encryption streaming

--- tests/test_pkcs11.py
+++ tests/test_pkcs11.py
@@ -110,14 +110,39 @@ class PKCS11Tests(unittest.TestCase):
     def test_aes_encrypt(self):
         lib = pkcs11.lib(LIB)
         token = next(lib.get_tokens(token_label='DEMO'))
+        data = b'INPUT DATA'
 
         with token.open(user_pin='1234') as session:
             key = session.generate_key(pkcs11.KeyType.AES, 128, store=False)
-            data = b'INPUT DATA'
             crypttext = key.encrypt(data, mechanism_param=b'0' * 16)
             self.assertIsInstance(crypttext, bytes)
             self.assertNotEqual(data, crypttext)
             # We should be aligned to the block size
             self.assertEqual(len(crypttext), 16)
+            # Ensure we didn't just get 16 nulls
+            self.assertFalse(all(c == '\0' for c in crypttext))
+
+    def test_aes_encrypt_stream(self):
+        lib = pkcs11.lib(LIB)
+        token = next(lib.get_tokens(token_label='DEMO'))
+        data = (
+            b'I' * 16,
+            b'N' * 16,
+            b'P' * 16,
+            b'U' * 16,
+            b'T' * 10,  # don't align to the blocksize
+        )
+
+        with token.open(user_pin='1234') as session:
+            key = session.generate_key(pkcs11.KeyType.AES, 128, store=False)
+            cryptblocks = list(key.encrypt(data, mechanism_param=b'0' * 16))
+
+            self.assertEqual(len(cryptblocks), len(data) + 1)
+
+            crypttext = b''.join(cryptblocks)
+
+            self.assertNotEqual(b''.join(data), crypttext)
+            # We should be aligned to the block size
+            self.assertEqual(len(crypttext) % 16, 0)
             # Ensure we didn't just get 16 nulls
             self.assertFalse(all(c == '\0' for c in crypttext))
