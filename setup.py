30f51601bc8136398844a76fee6c41bbf5b6ab8e
Danielle Madeley <danielle.madeley@squareweave.com.au>
2017-05-22 17:33:10 +1000

    Release prep

--- setup.py
+++ setup.py
@@ -2,35 +2,62 @@
 setup.py
 """
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
 
 from Cython.Build import cythonize
 
-ext_modules = [
-    Extension('pkcs11._loader',
-              sources=[
-                  'pkcs11/_loader.pyx',
-              ],
-    ),
-    Extension('pkcs11._pkcs11',
-              sources=[
-                  'pkcs11/_pkcs11.pyx',
-              ],
-              define_macros=[
-                  # These are required to build the PKCS11 headers
-                  #
-                  # They vary based on OS. See extern/pkcs11.h
-                  ('CK_PTR', '*'),
-                  ('CK_DEFINE_FUNCTION(returnType, name)', 'returnType name'),
-                  ('CK_DECLARE_FUNCTION(returnType, name)', 'returnType name'),
-                  ('CK_DECLARE_FUNCTION_POINTER(returnType, name)', 'returnType (* name)'),
-                  ('CK_CALLBACK_FUNCTION(returnType, name)', 'returnType (* name)'),
-              ],
-    ),
-]
+if __name__ == '__main__':
+    with \
+            open('requirements.in') as requirements, \
+            open('README.rst') as readme:
 
+        ext_modules = [
+            Extension('pkcs11._loader',
+                    sources=[
+                        'pkcs11/_loader.pyx',
+                    ],
+            ),
+            Extension('pkcs11._pkcs11',
+                    sources=[
+                        'pkcs11/_pkcs11.pyx',
+                    ],
+                    define_macros=[
+                        # These are required to build the PKCS11 headers
+                        #
+                        # They vary based on OS. See extern/pkcs11.h
+                        ('CK_PTR', '*'),
+                        ('CK_DEFINE_FUNCTION(returnType, name)', 'returnType name'),
+                        ('CK_DECLARE_FUNCTION(returnType, name)', 'returnType name'),
+                        ('CK_DECLARE_FUNCTION_POINTER(returnType, name)', 'returnType (* name)'),
+                        ('CK_CALLBACK_FUNCTION(returnType, name)', 'returnType (* name)'),
+                    ],
+            ),
+        ]
 
-setup(
-    ext_modules=cythonize(ext_modules),
-)
+        setup(
+            setup_requires=['setuptools_scm'],
+
+            name='python-pkcs11',
+            description='PKCS#11 (Cryptoki) support for Python',
+            use_scm_version=True,
+            author='Danielle Madeley',
+            author_email='danielle@madeley.id.au',
+            url='https://github.com/danni/python-pkcs11',
+            long_description=readme.read(),
+            classifiers=[
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Topic :: Security :: Cryptography',
+            ],
+
+            packages=find_packages(exclude=['tests']),
+            include_package_data=True,
+            ext_modules=cythonize(ext_modules),
+
+            install_requires=requirements.readlines(),
+            test_suite='tests',
+        )
