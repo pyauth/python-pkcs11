91ef07224d9741f39a2508669095277530beea84
Danielle Madeley <danielle.madeley@squareweave.com.au>
2017-07-04 19:28:34 +1000

    Port RSA utilities to asn1crypto

--- pkcs11/util/rsa.py
+++ pkcs11/util/rsa.py
@@ -1,13 +1,9 @@
 """
 Key handling utilities for RSA keys (PKCS#1).
-
-These utilities depend on :mod:`pyasn1` and :mod:`pyasn1_modules`.
 """
 
-from pyasn1.codec.der import encoder, decoder
-from pyasn1_modules.rfc2437 import RSAPrivateKey, RSAPublicKey
+from asn1crypto.keys import RSAPrivateKey, RSAPublicKey
 
-from . import biginteger
 from ..constants import Attribute, ObjectClass, MechanismFlag
 from ..mechanisms import KeyType
 from ..defaults import DEFAULT_KEY_CAPABILITIES
@@ -25,18 +21,19 @@ def decode_rsa_private_key(der, capabilities=None):
     if capabilities is None:
         capabilities = DEFAULT_KEY_CAPABILITIES[KeyType.RSA]
 
-    key, _ = decoder.decode(der, asn1Spec=RSAPrivateKey())
+    key = RSAPrivateKey.load(der)
+
     return {
         Attribute.CLASS: ObjectClass.PRIVATE_KEY,
         Attribute.KEY_TYPE: KeyType.RSA,
-        Attribute.MODULUS: biginteger(key['modulus']),
-        Attribute.PUBLIC_EXPONENT: biginteger(key['publicExponent']),
-        Attribute.PRIVATE_EXPONENT: biginteger(key['privateExponent']),
-        Attribute.PRIME_1: biginteger(key['prime1']),
-        Attribute.PRIME_2: biginteger(key['prime2']),
-        Attribute.EXPONENT_1: biginteger(key['exponent1']),
-        Attribute.EXPONENT_2: biginteger(key['exponent2']),
-        Attribute.COEFFICIENT: biginteger(key['coefficient']),
+        Attribute.MODULUS: key['modulus'],
+        Attribute.PUBLIC_EXPONENT: key['public_exponent'],
+        Attribute.PRIVATE_EXPONENT: key['private_exponent'],
+        Attribute.PRIME_1: key['prime1'],
+        Attribute.PRIME_2: key['prime2'],
+        Attribute.EXPONENT_1: key['exponent1'],
+        Attribute.EXPONENT_2: key['exponent2'],
+        Attribute.COEFFICIENT: key['coefficient'],
         Attribute.DECRYPT: MechanismFlag.DECRYPT in capabilities,
         Attribute.SIGN: MechanismFlag.SIGN in capabilities,
         Attribute.UNWRAP: MechanismFlag.UNWRAP in capabilities,
@@ -56,12 +53,12 @@ def decode_rsa_public_key(der, capabilities=None):
     if capabilities is None:
         capabilities = DEFAULT_KEY_CAPABILITIES[KeyType.RSA]
 
-    key, _ = decoder.decode(der, asn1Spec=RSAPublicKey())
+    key = RSAPublicKey.load(der)
     return {
         Attribute.CLASS: ObjectClass.PUBLIC_KEY,
         Attribute.KEY_TYPE: KeyType.RSA,
-        Attribute.MODULUS: biginteger(key['modulus']),
-        Attribute.PUBLIC_EXPONENT: biginteger(key['publicExponent']),
+        Attribute.MODULUS: key['modulus'],
+        Attribute.PUBLIC_EXPONENT: key['public_exponent'],
         Attribute.ENCRYPT: MechanismFlag.ENCRYPT in capabilities,
         Attribute.VERIFY: MechanismFlag.VERIFY in capabilities,
         Attribute.WRAP: MechanismFlag.WRAP in capabilities,
@@ -75,9 +72,8 @@ def encode_rsa_public_key(key):
     :param PublicKey key: RSA public key
     :rtype: bytes
     """
-    asn1 = RSAPublicKey()
-    asn1['modulus'] = int.from_bytes(key[Attribute.MODULUS],
-                                     byteorder='big')
-    asn1['publicExponent'] = int.from_bytes(key[Attribute.PUBLIC_EXPONENT],
-                                            byteorder='big')
-    return encoder.encode(asn1)
+    return RSAPublicKey({
+        'modulus': int.from_bytes(key[Attribute.MODULUS], byteorder='big'),
+        'public_exponent': int.from_bytes(key[Attribute.PUBLIC_EXPONENT],
+                                          byteorder='big'),
+    }).dump()
