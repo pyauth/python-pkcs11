4f5c57af01ed2aea39495d381bec3e440f7783c1
Danielle Madeley <danielle@madeley.id.au>
2020-02-28 13:30:44 +1100

    Remove Python 3.4 from supported languages

--- setup.py
+++ setup.py
@@ -39,9 +39,10 @@ if __name__ == '__main__':
                 'License :: OSI Approved :: MIT License',
                 'Programming Language :: Python',
                 'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.4',
                 'Programming Language :: Python :: 3.5',
                 'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+                'Programming Language :: Python :: 3.8',
                 'Topic :: Security :: Cryptography',
             ],
 
