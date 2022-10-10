818cd9c60d9b18241327900967496f58f6bf99f4
Jose Vega <joseavegaa1@gmail.com>
2022-10-10 00:04:02 -0600

    Updated setup.py and added pyproject.toml

--- setup.py
+++ setup.py
@@ -3,59 +3,32 @@
 setup.py
 """
 
-from setuptools import setup, find_packages
-from setuptools.extension import Extension
-import platform
+from setuptools import Extension, setup
+import sys
 
-# if compiling using MSVC, we need to link against user32 library
-if platform.system() == 'Windows':
-    libraries = ['user32',]
-else:
-    libraries = []
+def get_libraries(ls: bool = False) -> tuple|list:
+    """
+    Checks the OS and return link to user32 if on Windows
 
-if __name__ == '__main__':
-    with \
-            open('requirements.in') as requirements, \
-            open('README.rst') as readme:
+    Args:
+        - `ls`: Indicate if return a list or not.  
+    """
+    if sys.platform == 'win32':
+        if ls is True:
+            return ['user32',]
+        else:
+            return ('user32',)
+    else:
+        if ls is True:
+            return []
+        else:
+            return ()
 
-        ext_modules = [
-            Extension('pkcs11._pkcs11',
-                    sources=[
-                        'pkcs11/_pkcs11.pyx',
-                    ],
-                    libraries=libraries,
-            ),
-        ]
-
-        setup(
-            name='python-pkcs11',
-            description='PKCS#11 (Cryptoki) support for Python',
-            use_scm_version=True,
-            author='Danielle Madeley',
-            author_email='danielle@madeley.id.au',
-            url='https://github.com/danni/python-pkcs11',
-            long_description=readme.read(),
-            classifiers=[
-                'License :: OSI Approved :: MIT License',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.5',
-                'Programming Language :: Python :: 3.6',
-                'Programming Language :: Python :: 3.7',
-                'Programming Language :: Python :: 3.8',
-                'Topic :: Security :: Cryptography',
-            ],
-
-            packages=find_packages(exclude=['tests']),
-            include_package_data=True,
-            ext_modules=ext_modules,
-
-            install_requires=requirements.readlines(),
-            setup_requires=[
-                'cython',
-                'setuptools >= 18.0',
-                'setuptools_scm',
-            ],
-
-            test_suite='tests',
-        )
+try:
+    setup(ext_modules=[Extension(name = "pkcs11._pkcs11", 
+                                sources = ["pkcs11/_pkcs11.pyx"],
+                                libraries = get_libraries())])
+except:
+    setup(ext_modules=[Extension(name = "pkcs11._pkcs11", 
+                                sources = ["pkcs11/_pkcs11.pyx"],
+                                libraries = get_libraries(ls=True))])
