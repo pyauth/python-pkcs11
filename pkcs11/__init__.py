03db4e529a09982c1370db44154e00bab12b3cd1
Jacob Rodgers <jacob.rodgers@ravenind.com>
2023-11-21 13:07:35 -0700

    add util imports so examples are usable

--- pkcs11/__init__.py
+++ pkcs11/__init__.py
@@ -6,6 +6,11 @@ from .constants import *  # noqa: F403
 from .exceptions import *  # noqa: F403
 from .mechanisms import *  # noqa: F403
 from .types import *  # noqa: F403
+from .util import dh # noqa: F403
+from .util import dsa # noqa: F403
+from .util import ec # noqa: F403
+from .util import rsa # noqa: F403
+from .util import x509 # noqa: F403
 
 _so = None
 _lib = None
